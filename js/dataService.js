acdc92ebefb45f4b8d6e50a814fb7ec60aa10d53
Daniel Baker <dbaker731@gmail.com>
2016-09-23 16:15:53 -0500

    finishing the project

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,6 @@
 angular.module( 'quoteBook' ).service( 'dataService', function( $cookies ){
 
+  //creating an array of quote objects
   var quotes = [
     { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
     { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
@@ -11,23 +12,55 @@ angular.module( 'quoteBook' ).service( 'dataService', function( $cookies ){
   ];
 
 
+  // creating a cookieQuote varibale to hold the cookie of the quotes array
+  var cookieQuote = $cookies.getObject('quoteCookie');
+
+  // creating a function to push the quote array to the mainCtrl
   this.getData = function(){
-    return quotes;
+    // running an if statement to check if the cookie exists
+    if ( cookieQuote ) {
+      //returning the cookie quote array if it exists
+      return cookieQuote;
+    } else{
+      //returning the original array if there are no cookies
+      return quotes;
+    }
   };
 
+// creating a function to add quotes
   this.addData = function( quote ){
     if( quote.text && quote.author ){
       quotes.push( quote );
+      // creating a cookie to store the array
+      $cookies.putObject('quoteCookie', quotes);
+
+      // allowing the cookie array to accept new quotes
+      if ( cookieQuote ) {
+        cookieQuote.push ( quote );
+        // updating the cookie to reflect the new quote
+        $cookies.putObject('quoteCookie', cookieQuote);
+      }
+
       return true;
     }
     return false;
   };
 
+  // creating a function to delete quotes
   this.removeData = function( quoteText ){
     for (var i = 0; i < quotes.length; i++) {
       if (quotes[i].text.toLowerCase() === quoteText.toLowerCase() ) {
         quotes.splice( i, 1 );
       }
+    // deleting from the cookie array
+    for (var j = 0; j < cookieQuote.length; j++) {
+      if (cookieQuote[j].text.toLowerCase() === quoteText.toLowerCase()) {
+        cookieQuote.splice( j, 1 );
+        // updating the cookie
+        $cookies.putObject('quoteCookie', cookieQuote);
+      }
+    }
+
     }
   };
 
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,20 +1,14 @@
-angular.module( 'quoteBook' ).controller( 'mainCtrl', function( $scope, dataService, $cookies ){
+angular.module( 'quoteBook' ).controller( 'mainCtrl', function( $scope, dataService ){
 
   $scope.quotes = dataService.getData();
 
-  $scope.newQuoteText = $cookies.get('test1');
-  $scope.newQuoteAuthor = $cookies.get('test2');
-
-
   $scope.addQuote = function( val1, val2 ){
-
-    $cookies.put( 'test1', val1 );
-    $cookies.put( 'test2', val2 );
 
     var newQuote = {
       text: $scope.newQuoteText,
       author: $scope.newQuoteAuthor
     };
+
     if (dataService.addData( newQuote ) ) {
       $scope.newQuoteText = '';
       $scope.newQuoteAuthor = '';
@@ -26,10 +20,8 @@ angular.module( 'quoteBook' ).controller( 'mainCtrl', function( $scope, dataServ
     dataService.removeData( deleteText );
   };
 
-  // $scope.myCookieVal = $cookies.get('cookie');
-  //
-  // $scope.setCookie = function(val){
-  //   $cookies.put('cookie', val);
-  // };
+  $scope.clearCookies = function(){
+
+  };
 
 });
